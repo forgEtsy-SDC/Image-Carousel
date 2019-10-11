b4c06671dd726e62dd6c81441258ebd009deb640
Tye <tyemacon@gmail.com>
2019-10-11 10:22:57 -0500

    Database properly seeded

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require('express')
-
+const db = require('../database/db')
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
 let port = 3003;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 })
+
 
 module.exports.port = port;
