3e3278694351b26294ac2806b93d192f0f8ceacd
Tye Macon <tyemacon@gmail.com>
2019-10-19 13:01:04 -0500

    main image keeps maximum height

--- client/components/MainImage/MainImage.jsx
+++ client/components/MainImage/MainImage.jsx
@@ -3,11 +3,18 @@ import Style from './MainImage.css';
 import Arrow from '../Arrow/Arrow.jsx';
 
 const MainImage = ({ url, scrollLeft, scrollRight }) => {
+    const style = {
+        backgroundImage: `url(${url})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        width: '100%',
+        maxHeight: '100%',
+    }
     return (
-        <div className={Style.mainimage}>
+        <div style={style}>
             {<Arrow direction={1} onclick={scrollLeft} char="&#8592;"/>}
             {<Arrow direction={0} onclick={scrollRight} char="&#8594;"/>}
-            <img src={url}/>
+            {/* <img src={url}/> */}
         </div>
     )
 }
