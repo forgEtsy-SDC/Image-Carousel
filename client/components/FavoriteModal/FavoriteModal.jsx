6454fe01dbbf4e38a20ba51bd3a8d1f9ac2c3dcd
Tye <tyemacon@gmail.com>
2019-10-24 16:10:29 -0500

    favorite modal finished

--- client/components/FavoriteModal/FavoriteModal.jsx
+++ client/components/FavoriteModal/FavoriteModal.jsx
@@ -5,14 +5,35 @@ import Style from './FavoriteModal.css'
 ReactModal.setAppElement('body');
 
 const FavoriteModal = ({ favorited, unfavorited }) => {
-  return (
-    <div>
-      <ReactModal className={Style.modal} 
-        isOpen={favorited || unfavorited}>
-        {favorited ? <div>Added to favorites!</div> : <div>Removed from favorites D:</div>}
-      </ReactModal>
-    </div>
-  )
+  if(favorited || unfavorited){
+    return (
+      <div className={Style.modal}>
+        {favorited ?
+          <div className={Style.favorite}>
+            <p>&#10004; Favorited</p>
+          </div> 
+          :
+          <div className={Style.unfavorite}>
+            <p>Removed from favorites</p>
+          </div>
+        }
+        {favorited && <p className={Style.add}>Add to list</p>}
+      </div>
+    )
+  }else{
+    return null
+  }
+
+
+
+  // return (
+  //   <div>
+  //     <ReactModal className={Style.modal} 
+  //       isOpen={favorited || unfavorited}>
+  //       {favorited ? <div>Added to favorites!</div> : <div>Removed from favorites D:</div>}
+  //     </ReactModal>
+  //   </div>
+  // )
 }
 
 export default FavoriteModal;
